2c052af713d4f33583b256637bfacd011bd38ff7
Andrew Thomas <me@andrewmthomas.com>
2015-05-11 14:08:19 -0500

    A first commit?

--- Doing.hs
+++ Doing.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Doing (
-  doing 
+  doing
+, cut 
 ) where
+
+import Data.IntSet (empty, insert, member)
 
 doing :: Int -> [Int]
 doing x
@@ -10,3 +13,12 @@ doing x
  | x `mod` 3 == 0 = x:(doing (div x 3))
  | x `mod` 3 == 1 = x:(doing (x-10))
  | otherwise = x:(doing (x^2-1))
+
+cut :: [Int] -> [Int]
+cut (x:xs) = cut' (x:xs) [] empty
+ where
+  cut' [] zs iSet = zs
+  cut' (x:xs) zs iSet 
+   | x `member` iSet = zs
+   | otherwise = cut' xs (x:zs) (insert x iSet) 
+
