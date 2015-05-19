885b50a4281115640ba68b54e79e90052230ee79
Andrew Thomas <me@andrewmthomas.com>
2015-05-18 23:49:36 -0500

    Added primality test to doing.py

--- doing.py
+++ doing.py
@@ -1,4 +1,5 @@
 from math import floor, sqrt, ceil
+import itertools, operator, functools
 
 def gsieve(n):
 	m=floor(n/2)-1
@@ -7,7 +8,13 @@ def gsieve(n):
 		sequence[((i**2-3)//2)::i]=[False]*ceil((n-i**2)/(2*i))
 	for j, prime in enumerate(sequence):
 		if prime: yield 2*j+3
-		
-def lsieve(n):
-	return list(gsieve(n))
+	
+def sprime(n):
+	m=int(sqrt(n))+1
+	lesssquare=filter(lambda p: n%p==0, gsieve(m))
+	try:
+		next(lesssquare)
+		return False
+	except StopIteration:
+		return True
 
